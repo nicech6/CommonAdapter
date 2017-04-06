a193b84fb6bd2978cf6353ba2e5f6771de250086
cuihai <cuihai_lte@163.com>
2017-04-06 22:13:17 +0800

    增加开源引用库

--- library/src/androidTest/java/com/cuihai/library/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/cuihai/library/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.cuihai.library", appContext.getPackageName());
+        assertEquals("com.cuihai.library.test", appContext.getPackageName());
     }
 }

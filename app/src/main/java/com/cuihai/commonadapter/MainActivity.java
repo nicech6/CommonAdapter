07f66dc142f1a5d4722f3fef1b5d1f972a8d560f
cuihai <cuihai_lte@163.com>
2017-04-09 11:27:14 +0800

    增加sample

--- app/src/main/java/com/cuihai/commonadapter/MainActivity.java
+++ app/src/main/java/com/cuihai/commonadapter/MainActivity.java
@@ -2,36 +2,11 @@ package com.cuihai.commonadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.view.LayoutInflater;
-
-import com.cuihai.library.GCommonRVAdapter;
-import com.cuihai.library.GRecyclerView;
-import com.cuihai.library.GViewHolder;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    GRecyclerView recyclerView;
-    GCommonRVAdapter<String> adapter;
-    List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for (int i = 0; i < 100; i++) {
-            list.add("GRecyclerView" + i);
-        }
-        recyclerView = (GRecyclerView) findViewById(R.id.rv);
-        adapter = new GCommonRVAdapter<String>(this, R.layout.item_rv, list) {
-            @Override
-            public void convert(GViewHolder gViewHolder, String s, int position) {
-                gViewHolder.setText(R.id.tv, list.get(position).toString());
-            }
-        };
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
